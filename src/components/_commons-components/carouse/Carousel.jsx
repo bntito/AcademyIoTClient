4c23953b23b7961bf25a123397a9df1ae77477e7
Bruno Tito <bruno.nicolas.tito@gmail.com>
2024-05-30 19:57:36 -0300

    validationschema dni, phone. carousel inciacion

--- src/components/_commons-components/carouse/Carousel.jsx
+++ src/components/_commons-components/carouse/Carousel.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import banner1 from '../../../assets/main/banner1.jpg';
 import banner2 from '../../../assets/main/banner2.jpg';
 import banner3 from '../../../assets/main/banner3.jpg';
 
 import './carousel.css';
 
-function Carouse() {
+function Carousel() {
+  useEffect(() => {
+    const carouselElement = document.querySelector('#carouselExampleDark');
+    if (carouselElement) {
+      new bootstrap.Carousel(carouselElement, {
+        interval: 3500,
+        ride: 'carousel'
+      });
+    }
+  }, []);
+
   return (
-    <>
     <div className='container-carousel'>
       <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
         <div className="carousel-indicators">
@@ -17,21 +26,21 @@ function Carouse() {
         </div>
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="3500">
-            <img src={banner1} className="d-block w-100" alt="..."/>
+            <img src={banner1} className="d-block w-100" alt="Banner 1"/>
             <div className="carousel-caption d-none d-md-block">
               <h5 className='title-caption'>Estudia Códigos!</h5>
               <p className='sub-title-caption'>Todo en sistemas para dispositivos con códigos</p>
             </div>
           </div>
           <div className="carousel-item" data-bs-interval="3500">
-            <img src={banner2} className="d-block w-100" alt="..."/>
+            <img src={banner2} className="d-block w-100" alt="Banner 2"/>
             <div className="carousel-caption d-none d-md-block">
               <h5 className='title-caption'>Estudia Desarrollo!</h5>
               <p className='sub-title-caption'>Curso dedicado al manejo de las herramientas de la actualidad</p>
             </div>
           </div>
           <div className="carousel-item" data-bs-interval="3500">
-            <img src={banner3} className="d-block w-100" alt="..."/>
+            <img src={banner3} className="d-block w-100" alt="Banner 3"/>
             <div className="carousel-caption d-none d-md-block">
               <h5 className='title-caption'>Estudia Diseño del Desarrollo!</h5>
               <p className='sub-title-caption'>Lleva tu imaginación al límite y registralo en la vida real</p>
@@ -48,8 +57,7 @@ function Carouse() {
         </button>
       </div>
     </div>
-    </>
   );
 };
 
-export default Carouse;
+export default Carousel;
--- src/services/validations/validationSchema.js
+++ src/services/validations/validationSchema.js
@@ -1,65 +1,63 @@
 const validationSchema = {
   code: [
-    (value) => value.trim() === "" ? "El código del curso es requerido" : undefined
+    (value) => value.trim() === "" ? "El código del curso es obligatorio" : undefined
   ],
   name: [
-    (value) => value.trim() === "" ? "El nombre es requerido" : undefined,
-    (value) => value.length < 3 ? "Nombre debe tener almenos 3 caracteres" : undefined
+    (value) => value.trim() === "" ? "El nombre es obligatorio" : undefined,
+    (value) => value.length < 3 ? "El nombre debe tener al menos 3 caracteres" : undefined
   ],
   cost: [
-    (value) => value.trim() === "" ? "El costo del curso es requerido" : undefined
+    (value) => value.trim() === "" ? "El costo del curso es obligatorio" : undefined
   ],
   duration: [
-    (value) => String(value).trim() === "" ? "La duración del curso es requerida" : undefined
+    (value) => String(value).trim() === "" ? "La duración del curso es obligatoria" : undefined
   ],
   qualification: [
-    (value) => String(value).trim() === "" ? "La calificación del curso es requerida" : undefined
+    (value) => String(value).trim() === "" ? "La calificación del curso es obligatoria" : undefined
   ],
   description: [
-    (value) => value.trim() === "" ? "La descripción es requerida" : undefined,
-    (value) => value.length < 3 ? "La descripción debe tener almenos 3 caracteres" : undefined
+    (value) => value.trim() === "" ? "La descripción es obligatoria" : undefined,
+    (value) => value.length < 3 ? "La descripción debe tener al menos 3 caracteres" : undefined
   ],
-
   dni: [
-    (value) => value.trim() === "" ? "El número de documento es requerido" : undefined,
-    (value) => value.length < 8 ? "El DNI debe tener almenos 8 números" : undefined,
-    (value) => value.match("^[0-9]+$") === null ? "Solo se admiten números" : undefined
-  ],
+    (value) => value.trim() === "" ? "El número de documento es obligatorio" : undefined,
+    (value) => !/^\d{1,3}(\.\d{3}){2}-\d{1}$/.test(value) ? "El formato del DNI debe ser X.XXX.XXX-X" : undefined
+  ],  
   lastname: [
-    (value) => value.trim() === "" ? "El apeliido es requerido" : undefined,
-    (value) => value.length < 3 ? "Apellido debe tener almenos 3 caracteres" : undefined
+    (value) => value.trim() === "" ? "El apellido es obligatorio" : undefined,
+    (value) => value.length < 3 ? "El apellido debe tener al menos 3 caracteres" : undefined
   ],
   email: [
-    (value) => value.trim() === "" ? "El email es requerido" : undefined,
-    (value) => !/^\S+@\S+\S.$/.test(value) ? "Email no es válido" : undefined
+    (value) => value.trim() === "" ? "El correo electrónico es obligatorio" : undefined,
+    (value) => !/^\S+@\S+\S.$/.test(value) ? "El correo electrónico no es válido" : undefined
   ],
   phone: [
-    (value) => value.trim() === "" ? "El celular es requerido" : undefined,
-    (value) => value.match("^[0-9]+$") === null ? "Solo se admiten números" : undefined,
-    (value) => value.length < 9 ? "El celular debe tener almenos 9 números" : undefined
+    (value) => value.trim() === "" ? "El número de teléfono es obligatorio" : undefined,
+    (value) => value.length < 9 ? "El número de teléfono debe tener al menos 9 dígitos" : undefined,
+    (value) => !/^0\d{2}-\d{3}-\d{3}$/.test(value) ? "El formato del número de teléfono debe ser 0XX-XXX-XXX" : undefined
   ],
   password: [
-    (value) => value.trim() === "" ? "La contraseña es requerida" : undefined,
-    (value) => value.length < 6 ? "La contraseña debe tener almenos 6 caracteres" : undefined
+    (value) => value.trim() === "" ? "La contraseña es obligatoria" : undefined,
+    (value) => value.length < 6 ? "La contraseña debe tener al menos 6 caracteres" : undefined
   ],
   confirmPassword: [
-    (value, formData) => value.trim() === "" ? "La confirmación de la contraseña es requerida" : undefined,
+    (value, formData) => value.trim() === "" ? "La confirmación de la contraseña es obligatoria" : undefined,
     (value, formData) => value !== formData.password ? "Las contraseñas no coinciden" : undefined
   ],
   newPassword: [
-    (value) => value.trim() === "" ? "La contraseña es requerida" : undefined,
-    (value) => value.length < 6 ? "La contraseña debe tener almenos 6 caracteres" : undefined
+    (value) => value.trim() === "" ? "La nueva contraseña es obligatoria" : undefined,
+    (value) => value.length < 6 ? "La nueva contraseña debe tener al menos 6 caracteres" : undefined
   ],
   newConfirmPassword: [
-    (value, formData) => value.trim() === "" ? "La confirmación de la contraseña es requerida" : undefined,
-    (value, formData) => value !== formData.newPassword ? "Las contraseñas no coinciden" : undefined
+    (value, formData) => value.trim() === "" ? "La confirmación de la nueva contraseña es obligatoria" : undefined,
+    (value, formData) => value !== formData.newPassword ? "Las nuevas contraseñas no coinciden" : undefined
   ],
   address: [
-    (value) => value.trim() === "" ? "La dirección es requerida" : undefined
+    (value) => value.trim() === "" ? "La dirección es obligatoria" : undefined
   ],
   message: [
-    (value) => value.trim() === "" ? "El comentario es requerido" : undefined,
-    (value) => value.length < 15 ? "El comentario debe tener almenos 15 caracteres" : undefined
+    (value) => value.trim() === "" ? "El comentario es obligatorio" : undefined,
+    (value) => value.length < 15 ? "El comentario debe tener al menos 15 caracteres" : undefined
   ]
 };
 
