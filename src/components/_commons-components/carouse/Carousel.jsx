a51ae6dcbe912fc173458f640a47e1747d5a20d7
Bruno Tito <bruno.nicolas.tito@gmail.com>
2024-07-11 06:53:51 -0300

    imagenes staticas

--- src/components/_commons-components/carouse/Carousel.jsx
+++ src/components/_commons-components/carouse/Carousel.jsx
@@ -1,45 +1,120 @@
+// import React, { useEffect, useState } from 'react';
+// import { useFetch } from '../../../hooks/useFetch';
+// import { Link } from 'react-router-dom';
+
+// import './carousel.css';
+
+// function Carousel() {
+//   // Host del servidor desde las variables de entorno y contexto de usuario
+//   const hostServer = import.meta.env.VITE_REACT_APP_SERVER_HOST;
+//   const api = `${hostServer}/api/courses`;
+//   const [cards, setCards] = useState([]);
+
+//   // Hook de fetch personalizado
+//   let {
+//     dataServer,
+//     getData
+//   } = useFetch(`${api}`);
+
+//   // Obtener cursos desde el servidor
+//   const getCourses = async () => {
+//     await getData(api);
+//   };
+
+//   // Efecto para obtener los cursos al cargar el componente y configurar el carrusel
+//   useEffect(() => {
+//     getCourses();
+//     const carouselElement = document.querySelector('#carouselExampleDark');
+//     if (carouselElement) {
+//       new bootstrap.Carousel(carouselElement, {
+//         interval: 3500,
+//         ride: 'carousel'
+//       });
+//     }
+//   }, []);
+
+//   // Efecto para actualizar el estado cuando se reciben los datos del servidor
+//   useEffect(() => {
+//     if (dataServer && dataServer.dataServerResult) {
+//       const allCards = dataServer.dataServerResult.dataApi;
+//       setCards(allCards);
+//     }
+//   }, [dataServer]);
+
+//   return (
+//     <div className='container-carousel'>
+//       <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+//         <div className="carousel-indicators">
+//           {
+//             cards.map((card, index) => (
+//               <button
+//                 key={index}
+//                 type="button"
+//                 data-bs-target="#carouselExampleDark"
+//                 data-bs-slide-to={index}
+//                 className={index === 0 ? 'active' : ''}
+//                 aria-current={index === 0 ? 'true' : 'false'}
+//                 aria-label={`Slide ${index + 1}`}
+//               >
+//               </button>
+//             ))
+//           }
+//         </div>
+//         <div className="carousel-inner">
+//           {
+//             cards.map((card, index) => (
+//               <div 
+//                 key={card.id}
+//                 className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval="3500"
+//               >
+//                 <Link to={`/courseView/${card.id}`}>
+//                   <img 
+//                     src={`${hostServer}${card.urlImg}`}
+//                     alt={`Banner ${index + 1}`} 
+//                     className="d-block w-100"
+//                   />
+//                   <div className="carousel-caption d-none d-md-block">
+//                     <h5 className='title-caption'>{card.name}</h5>
+//                     <p className='sub-title-caption'>{card.description}</p>
+//                   </div>
+//                 </Link>
+//               </div>
+//             ))
+//           }
+//         </div>
+//         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+//           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+//           <span className="visually-hidden">Previous</span>
+//         </button>
+//         <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+//           <span className="carousel-control-next-icon" aria-hidden="true"></span>
+//           <span className="visually-hidden">Next</span>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Carousel;
+
 import React, { useEffect, useState } from 'react';
-import { useFetch } from '../../../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import banner1 from '../../../assets/banner1.jpg';
+import banner2 from '../../../assets/banner2.jpg';
+import banner3 from '../../../assets/banner3.jpg';
 
 import './carousel.css';
 
 function Carousel() {
-  // Host del servidor desde las variables de entorno y contexto de usuario
-  const hostServer = import.meta.env.VITE_REACT_APP_SERVER_HOST;
-  const api = `${hostServer}/api/courses`;
   const [cards, setCards] = useState([]);
 
-  // Hook de fetch personalizado
-  let {
-    dataServer,
-    getData
-  } = useFetch(`${api}`);
-
-  // Obtener cursos desde el servidor
-  const getCourses = async () => {
-    await getData(api);
-  };
-
-  // Efecto para obtener los cursos al cargar el componente y configurar el carrusel
   useEffect(() => {
-    getCourses();
-    const carouselElement = document.querySelector('#carouselExampleDark');
-    if (carouselElement) {
-      new bootstrap.Carousel(carouselElement, {
-        interval: 3500,
-        ride: 'carousel'
-      });
-    }
+    setCards([
+      { id: 1, urlImg: banner1, name: 'Banner 1', description: 'Descripción 1' },
+      { id: 2, urlImg: banner2, name: 'Banner 2', description: 'Descripción 2' },
+      { id: 3, urlImg: banner3, name: 'Banner 3', description: 'Descripción 3' },
+    ]);
   }, []);
-
-  // Efecto para actualizar el estado cuando se reciben los datos del servidor
-  useEffect(() => {
-    if (dataServer && dataServer.dataServerResult) {
-      const allCards = dataServer.dataServerResult.dataApi;
-      setCards(allCards);
-    }
-  }, [dataServer]);
 
   return (
     <div className='container-carousel'>
@@ -48,7 +123,7 @@ function Carousel() {
           {
             cards.map((card, index) => (
               <button
-                key={index}
+                key={card.id}
                 type="button"
                 data-bs-target="#carouselExampleDark"
                 data-bs-slide-to={index}
@@ -67,17 +142,15 @@ function Carousel() {
                 key={card.id}
                 className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval="3500"
               >
-                <Link to={`/courseView/${card.id}`}>
-                  <img 
-                    src={`${hostServer}${card.urlImg}`}
-                    alt={`Banner ${index + 1}`} 
-                    className="d-block w-100"
-                  />
-                  <div className="carousel-caption d-none d-md-block">
-                    <h5 className='title-caption'>{card.name}</h5>
-                    <p className='sub-title-caption'>{card.description}</p>
-                  </div>
-                </Link>
+                <img 
+                  src={card.urlImg}
+                  alt={`Banner ${index + 1}`} 
+                  className="d-block w-100"
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5 className='title-caption'>{card.name}</h5>
+                  <p className='sub-title-caption'>{card.description}</p>
+                </div>
               </div>
             ))
           }
--- src/components/_commons-components/home/CardCourse.jsx
+++ src/components/_commons-components/home/CardCourse.jsx
@@ -1,43 +1,133 @@
+// import { useEffect, useState } from 'react';
+// import { useFetch } from '../../../hooks/useFetch';
+// import { Link } from 'react-router-dom';
+
+// import './cards.css';
+
+// function CardCourses() {
+//   // Host del servidor desde las variables de entorno y contexto de usuario
+//   const hostServer = import.meta.env.VITE_REACT_APP_SERVER_HOST;
+//   const api = `${hostServer}/api/courses`;
+
+//   // Estados locales
+//   const [cards, setCards] = useState([]);
+//   const [startIndex, setStartIndex] = useState(0);
+
+//   // Hook de fetch personalizado
+//   let {
+//     dataServer,
+//     isLoading = false,
+//     getData
+//   } = useFetch(`${api}`);
+
+//   // Obtener cursos desde el servidor
+//   const getCourses = async () => {
+//     await getData(api);
+//   };
+
+//   // Obtener lista de cursos al montar el componente
+//   useEffect(() => {
+//     getCourses();
+//   }, []);
+
+//   // Actualiza las tarjetas destacadas en función de los datos del servidor
+//   useEffect(() => {
+//     if (dataServer && dataServer.dataServerResult) {
+//       const allCards = dataServer.dataServerResult.dataApi
+//       const prominentCards = allCards.filter(card => card.prominent === true);
+//       setCards(prominentCards);
+//     }
+//   }, [dataServer]);
+
+//   // Temporizador para cambiar las tarjetas cada 3 segundos
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       setStartIndex((prevIndex) => (prevIndex + 4) % cards.length);
+//     }, 3000);
+
+//     return () => {
+//       clearInterval(timer);
+//     };
+//   }, [cards]);
+
+//   return (
+//     <>
+//       <h1 className='title-b2'>Cursos <b>Destacados</b></h1>
+//       <hr />
+//       {
+//         isLoading ? (
+//           <h3>Cargando...</h3>
+//         ) : (
+//           <div className='row'>
+//             <div className='cards cards-home'>
+//               {
+//                 cards.slice(startIndex, startIndex + 4).map((card) => (
+//                   <Link
+//                     key={card.id}
+//                     to={`/courseView/${card.id}`}
+//                     className='card card-home h-100 card'
+//                   >
+//                     <img
+//                       src={`${hostServer}${card.urlImg}`}
+//                       alt={card.description}
+//                       className='card-img-top'
+//                     />
+//                     <div className='card-body'>
+//                       <h5 className='card-title'>{card.name}</h5>
+//                       <p className='card-text'>{card.description}</p>
+//                     </div>
+//                     <div className='card-footer'>
+//                       <div className='grup-footer'>
+//                         <small className='text-muted'>{`Duración de los cursos ${card.duration}`}</small>
+//                         <div>
+//                           <span>{`Clasificación de ${card.qualification} Estrellas`}</span>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </Link>
+//                 ))
+//               }
+//             </div>
+//           </div>
+//         )
+//       }
+//     </>
+//   );
+// }
+
+// export default CardCourses;
+
 import { useEffect, useState } from 'react';
-import { useFetch } from '../../../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import java from '../../../assets/java.webp';
+import javascript from '../../../assets/javascript.jpg';
+import MongoDB from '../../../assets/MongoDB.jpg';
+import mysql from '../../../assets/mysql.png';
+import php from '../../../assets/php.webp';
+import python from '../../../assets/python.webp';
+import react from '../../../assets/react.png';
+import vue from '../../../assets/vue.jpg';
 
 import './cards.css';
 
 function CardCourses() {
-  // Host del servidor desde las variables de entorno y contexto de usuario
-  const hostServer = import.meta.env.VITE_REACT_APP_SERVER_HOST;
-  const api = `${hostServer}/api/courses`;
-
   // Estados locales
   const [cards, setCards] = useState([]);
   const [startIndex, setStartIndex] = useState(0);
 
-  // Hook de fetch personalizado
-  let {
-    dataServer,
-    isLoading = false,
-    getData
-  } = useFetch(`${api}`);
-
-  // Obtener cursos desde el servidor
-  const getCourses = async () => {
-    await getData(api);
-  };
-
-  // Obtener lista de cursos al montar el componente
+  // Inicializar las tarjetas con imágenes importadas
   useEffect(() => {
-    getCourses();
+    setCards([
+      { id: 1, urlImg: java, name: 'Java', description: 'Curso de Java', duration: '3 meses', qualification: '4.5' },
+      { id: 2, urlImg: javascript, name: 'JavaScript', description: 'Curso de JavaScript', duration: '2 meses', qualification: '4.0' },
+      { id: 3, urlImg: MongoDB, name: 'MongoDB', description: 'Curso de MongoDB', duration: '4 meses', qualification: '5.0' },
+      { id: 4, urlImg: mysql, name: 'MySQL', description: 'Curso de MySQL', duration: '1 mes', qualification: '3.5' },
+      { id: 5, urlImg: php, name: 'PHP', description: 'Curso de PHP', duration: '3 meses', qualification: '4.2' },
+      { id: 6, urlImg: python, name: 'Python', description: 'Curso de Python', duration: '2 meses', qualification: '4.8' },
+      { id: 7, urlImg: react, name: 'React', description: 'Curso de React', duration: '3 meses', qualification: '4.7' },
+      { id: 8, urlImg: vue, name: 'Vue', description: 'Curso de Vue', duration: '2 meses', qualification: '4.4' },
+    ]);
   }, []);
-
-  // Actualiza las tarjetas destacadas en función de los datos del servidor
-  useEffect(() => {
-    if (dataServer && dataServer.dataServerResult) {
-      const allCards = dataServer.dataServerResult.dataApi
-      const prominentCards = allCards.filter(card => card.prominent === true);
-      setCards(prominentCards);
-    }
-  }, [dataServer]);
 
   // Temporizador para cambiar las tarjetas cada 3 segundos
   useEffect(() => {
@@ -54,43 +144,37 @@ function CardCourses() {
     <>
       <h1 className='title-b2'>Cursos <b>Destacados</b></h1>
       <hr />
-      {
-        isLoading ? (
-          <h3>Cargando...</h3>
-        ) : (
-          <div className='row'>
-            <div className='cards cards-home'>
-              {
-                cards.slice(startIndex, startIndex + 4).map((card) => (
-                  <Link
-                    key={card.id}
-                    to={`/courseView/${card.id}`}
-                    className='card card-home h-100 card'
-                  >
-                    <img
-                      src={`${hostServer}${card.urlImg}`}
-                      alt={card.description}
-                      className='card-img-top'
-                    />
-                    <div className='card-body'>
-                      <h5 className='card-title'>{card.name}</h5>
-                      <p className='card-text'>{card.description}</p>
+      <div className='row'>
+        <div className='cards cards-home'>
+          {
+            cards.slice(startIndex, startIndex + 4).map((card) => (
+              <Link
+                key={card.id}
+                to={`/courseView/${card.id}`}
+                className='card card-home h-100 card'
+              >
+                <img
+                  src={card.urlImg}
+                  alt={card.description}
+                  className='card-img-top'
+                />
+                <div className='card-body'>
+                  <h5 className='card-title'>{card.name}</h5>
+                  <p className='card-text'>{card.description}</p>
+                </div>
+                <div className='card-footer'>
+                  <div className='grup-footer'>
+                    <small className='text-muted'>{`Duración de los cursos ${card.duration}`}</small>
+                    <div>
+                      <span>{`Clasificación de ${card.qualification} Estrellas`}</span>
                     </div>
-                    <div className='card-footer'>
-                      <div className='grup-footer'>
-                        <small className='text-muted'>{`Duración de los cursos ${card.duration}`}</small>
-                        <div>
-                          <span>{`Clasificación de ${card.qualification} Estrellas`}</span>
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                ))
-              }
-            </div>
-          </div>
-        )
-      }
+                  </div>
+                </div>
+              </Link>
+            ))
+          }
+        </div>
+      </div>
     </>
   );
 }
